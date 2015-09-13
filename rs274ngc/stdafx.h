fa595ffab8b22218c2cb1046870635b5b2436de0
charliex <charlie@finitemonkeys.com>
2015-09-13 10:35:16 -0700

    initial commit mostly channged fopen/sscanf/fgets/gets etc added stdafx and VC2015 project.

---
+++ rs274ngc/stdafx.h
@@ -0,0 +1,24 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <conio.h>
+#include <io.h>
+#include "rs274ngc.h"
+#include "rs274ngc_return.h"
+extern char * _rs274ngc_errors[];
